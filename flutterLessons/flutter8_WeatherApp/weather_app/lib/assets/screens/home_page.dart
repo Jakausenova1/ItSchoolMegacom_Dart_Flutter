d0e23ccc8421049b6db8a9a0704821cd2eddad95
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2023-02-03 03:36:51 +0600

    w

--- flutterLessons/flutter8_WeatherApp/weather_app/lib/assets/screens/home_page.dart
+++ flutterLessons/flutter8_WeatherApp/weather_app/lib/assets/screens/home_page.dart
@@ -1,8 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/assets/app_colors.dart';
 import 'package:weather_app/assets/app_images.dart';
-
 import '../../providers/weather_provider.dart';
 import '../app_fonts.dart';
 
@@ -10,9 +10,8 @@ class WeatherPage extends StatelessWidget {
   WeatherPage({super.key});
 
   String apiKey = "ecd90844dbbf9ac0ad94178ad6f8631f";
-  String location = "Bishkek";
-  // String lat = "42.882004";
-  // String lon = "74.582748";
+  String location = "Paris";
+  Timer? timer;
 
   @override
   Widget build(BuildContext context) {
@@ -20,164 +19,189 @@ class WeatherPage extends StatelessWidget {
 
     vm.getWeather(apiKey: apiKey, location: location);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.darkBlue,
-        elevation: 0,
-        title: TextField(
-          style: AppFonts.w700s20.copyWith(color: AppColors.white),
-          decoration: InputDecoration(
-            hintText: "Search...",
-            hintStyle: AppFonts.w700s20.copyWith(color: AppColors.white),
-            prefixIcon: const Icon(
-              Icons.search,
-              size: 30,
-              color: AppColors.white,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [AppColors.darkBlue, AppColors.blue, AppColors.blue],
             ),
           ),
-        ),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [AppColors.darkBlue, AppColors.blue, AppColors.blue],
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(children: [
-            SizedBox(
-              height: 207,
-              width: 284,
-              child: Image.asset(AppImages.cloud),
-            ),
-            Text(
-              "${vm.model?.temp ?? "100"}°",
-              style: AppFonts.w600s64.copyWith(color: AppColors.white),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Precipitations",
-              style: AppFonts.w400s18.copyWith(color: AppColors.white),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Max.: ${vm.model?.tempMax ?? "100"}",
-                  style: AppFonts.w400s18.copyWith(color: AppColors.white),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "Min.: ${vm.model?.tempMin ?? "100"}",
-                  style: AppFonts.w400s18.copyWith(color: AppColors.white),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              width: 343,
-              height: 47,
-              decoration: const BoxDecoration(
-                color: AppColors.darkBlue,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: Row(
+          child: SingleChildScrollView(
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.only(left: 30),
-                    child: Icon(
-                      Icons.water_drop,
-                      size: 14,
-                      color: AppColors.white,
+                  SizedBox(
+                    width: 250,
+                    child: TextField(
+                      style: AppFonts.w700s20.copyWith(color: AppColors.white),
+                      onChanged: (val) {
+                        timer?.cancel();
+                        timer = Timer(
+                          const Duration(seconds: 1),
+                          () {
+                            (val.length >= 2 && location != val);
+                            vm.getWeather(apiKey: apiKey, location: val);
+                            location = val;
+                          },
+                        );
+                      },
                     ),
                   ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.darkBlue,
+                        fixedSize: const Size(30, 30),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.search,
+                        size: 30,
+                        color: AppColors.white,
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 207,
+                width: 284,
+                child: Image.asset(AppImages.cloud),
+              ),
+              Text(
+                "${vm.model?.temp ?? "100"}°",
+                style: AppFonts.w600s64.copyWith(color: AppColors.white),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Precipitations",
+                style: AppFonts.w400s18.copyWith(color: AppColors.white),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   Text(
-                    "${vm.model?.pressure ?? "5"}%",
-                    style: AppFonts.w700s14.copyWith(color: AppColors.white),
+                    "Max.: ${vm.model?.tempMax ?? "100"}",
+                    style: AppFonts.w400s18.copyWith(color: AppColors.white),
                   ),
                   const SizedBox(
-                    width: 50,
-                  ),
-                  const Icon(
-                    Icons.device_thermostat_outlined,
-                    size: 14,
-                    color: AppColors.white,
+                    width: 10,
                   ),
                   Text(
-                    "${vm.model?.humidity ?? "90"}%",
-                    style: AppFonts.w700s14.copyWith(color: AppColors.white),
-                  ),
-                  const SizedBox(
-                    width: 50,
-                  ),
-                  const Icon(
-                    Icons.air_outlined,
-                    size: 14,
-                    color: AppColors.white,
-                  ),
-                  Text(
-                    "${vm.model?.wind ?? "19"} km/h",
-                    style: AppFonts.w700s14.copyWith(color: AppColors.white),
+                    "Min.: ${vm.model?.tempMin ?? "100"}",
+                    style: AppFonts.w400s18.copyWith(color: AppColors.white),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
                 width: 343,
-                height: 200,
+                height: 47,
                 decoration: const BoxDecoration(
                   color: AppColors.darkBlue,
                   borderRadius: BorderRadius.all(Radius.circular(20)),
                 ),
-                child: Column(
+                child: Row(
                   children: [
-                    const SizedBox(
-                      height: 10,
+                    const Padding(
+                      padding: EdgeInsets.only(left: 30),
+                      child: Icon(
+                        Icons.water_drop,
+                        size: 14,
+                        color: AppColors.white,
+                      ),
                     ),
                     Text(
-                      "Today: ",
-                      style: AppFonts.w700s20.copyWith(color: AppColors.white),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "${vm.model?.temp ?? "100"}°",
+                      "${vm.model?.pressure ?? "5"}%",
                       style: AppFonts.w700s14.copyWith(color: AppColors.white),
                     ),
                     const SizedBox(
-                      height: 20,
+                      width: 50,
                     ),
-                    SizedBox(
-                      height: 50,
-                      child: Image.asset(AppImages.cloud),
-                    ),
-                    const SizedBox(
-                      height: 20,
+                    const Icon(
+                      Icons.device_thermostat_outlined,
+                      size: 14,
+                      color: AppColors.white,
                     ),
                     Text(
-                      "hours: ",
+                      "${vm.model?.humidity ?? "90"}%",
+                      style: AppFonts.w700s14.copyWith(color: AppColors.white),
+                    ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    const Icon(
+                      Icons.air_outlined,
+                      size: 14,
+                      color: AppColors.white,
+                    ),
+                    Text(
+                      "${vm.model?.wind ?? "19"} km/h",
                       style: AppFonts.w700s14.copyWith(color: AppColors.white),
                     ),
                   ],
-                )),
-          ]),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                  width: 343,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    color: AppColors.darkBlue,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Today: ",
+                        style:
+                            AppFonts.w700s20.copyWith(color: AppColors.white),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "${vm.model?.temp ?? "100"}°",
+                        style:
+                            AppFonts.w700s14.copyWith(color: AppColors.white),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: Image.asset(AppImages.cloud),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "hours: ",
+                        style:
+                            AppFonts.w700s14.copyWith(color: AppColors.white),
+                      ),
+                    ],
+                  )),
+            ]),
+          ),
         ),
       ),
     );
