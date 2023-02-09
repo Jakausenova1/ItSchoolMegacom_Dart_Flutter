da70a89c742e07ec52d25376288688cd2aff541d
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2023-02-10 01:15:56 +0600

    not finished

--- flutterLessons/flutter11/flutter_application_1/lib/welcome_phone.dart
+++ flutterLessons/flutter11/flutter_application_1/lib/welcome_phone.dart
@@ -7,57 +7,68 @@ class WelcomePhone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(AppImages.bg),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(AppImages.bg),
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 84),
-          child: RotatedBox(
-            quarterTurns: 4,
-            child: Image.asset(
-              AppImages.circle,
-              width: 215,
-              height: 215,
+          Padding(
+            padding: const EdgeInsets.only(top: 84),
+            child: RotatedBox(
+              quarterTurns: 4,
+              child: Image.asset(
+                AppImages.circle,
+                width: 215,
+                height: 215,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 84, top: 470),
-          child: RotatedBox(
-            quarterTurns: 1,
-            child: Image.asset(
-              AppImages.circle,
-              width: 284,
-              height: 284,
+          Padding(
+            padding: const EdgeInsets.only(left: 84, top: 470),
+            child: RotatedBox(
+              quarterTurns: 1,
+              child: Image.asset(
+                AppImages.circle,
+                width: 284,
+                height: 284,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 185),
-          child: Container(
-            width: 428,
-            height: 450,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: Colors.white.withOpacity(0.35),
-            ),
-            child: Column(
-              children: const [
-                SizedBox(
-                  height: 25,
-                ),
-              ],
+          Padding(
+            padding: const EdgeInsets.only(top: 185),
+            child: Container(
+              width: 428,
+              height: 450,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Colors.white.withOpacity(0.35),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text("Welcome",
+                      style: AppFonts.w600s25.copyWith(color: Colors.white)),
+                  SizedBox(
+                    width: 100,
+                    height: 50,
+                    child: TextField(
+                      decoration: InputDecoration(hintText: "dd"),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
